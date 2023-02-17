ec7167fb41cfefdeb0733aff1a37a720a61b6347
mvpsheng <2711136701@qq.com>
2023-02-17 17:55:04 +0800

    删除多余文件

--- imageUploaderBackend/schema/ads.js
+++
@@ -1,8 +0,0 @@
-exports = module.exports = function(app, mongoose) {
-    var addsSchema = new mongoose.Schema({
-      imageTitle     : { type: String, default: '' },    
-      img            : { type: String, default: '' }
-
-   });
-   app.db.model('Adds', addsSchema);
- };
--- imageUploaderBackend/server.js
+++
@@ -1,9 +0,0 @@
-var multer = require('multer')
-var upload = multer({dest : 'uploads/'}).single('photo') 
-//make sure you have access to write files
-//make sure 'photo' matches up with the field entered at the front end e.g. formdata.append('photo', ...)
-
-app.post('/imageupload',upload,function(req,res){
-    //req.file will now be available as a json object, save to mongodb, re: filename, path etc
-    res.send('rabbit')
-})
